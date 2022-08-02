55cbc069460b4b5e3ea65887d4389b5c1d9e80c3
Ifty <iftikharimrul@iut-dhaka.edu>
2022-08-02 21:59:57 +0600

    app req works

--- back/app.js
+++ back/app.js
@@ -1,13 +1,19 @@
 const express = require('express')
+const dotenv = require('dotenv')
 
 
 const app = express()
 
 
+dotenv.config()
+app.use(express.json())
+
+
+app.get('/', (req, res)=> {
+    res.status(200).send('req sending working!');
+})
 
 
 
-
-
-const port = 3000
+const port = process.env.PORT
 app.listen(port, ()=> console.log(`Server Listening on port ${port}`))
