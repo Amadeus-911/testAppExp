57f616cc1890c4b4a68a050e628c06f04037392d
Ifty <iftikharimrul@iut-dhaka.edu>
2022-09-21 23:29:39 +0600

    new

--- back/routes/adminRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const adminRouter = express.Router()
-
-adminRouter.post('/', (req, res) => {
-  res.send(req.body)
-});
-
-module.exports = adminRouter
--- back/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const userRouter = express.Router()
-
-userRouter.post('/', (req, res) => {
-  res.send(req.body)
-});
-
-module.exports = userRouter
